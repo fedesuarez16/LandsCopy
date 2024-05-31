ac528148646bd7b49b8f2c3e2f97cf5d30b4c954
fedesuarez16 <fedesuarez16@gmail.com>
2024-05-31 18:53:52 -0300

    idk

--- src/components/WhatsappButton.jsx
+++ src/components/WhatsappButton.jsx
@@ -3,32 +3,32 @@ import { BsChevronDown } from 'react-icons/bs'; // Puedes usar cualquier icono d
 import { wsp } from "../assets";
 
 const ScrollAndWhatsAppButton = () => {
-  const [isVisible, setIsVisible] = useState(true);
+  const [currentSection, setCurrentSection] = useState(0);
+  const sections = ['#nosotros', '#premisas', '#management', '#lineas', "#negocios", "#entretenimiento", "#alianzass", "#contacto", "#hero" ]; // Lista de IDs de las secciones
 
   const handleScroll = () => {
-    window.scrollTo({
-      top: window.scrollY + window.innerHeight, // Desplaza hacia abajo una altura de ventana completa desde la posición actual
+    const nextSection = (currentSection + 1) % sections.length; // Calcula el siguiente índice
+    document.querySelector(sections[nextSection]).scrollIntoView({
       behavior: 'smooth',
     });
+    setCurrentSection(nextSection); // Actualiza el índice actual
   };
 
   return (
     <div className="fixed z-50 right-0 bottom-0 flex flex-col items-center m-4 space-y-2">
-      {isVisible && (
-        <div
-          onClick={handleScroll}
-          className="p-4 bg-white opacity-80 rounded-full text-gray-500 cursor-pointer flex items-center justify-center"
-        >
-          <BsChevronDown size={20} />
-        </div>
-      )}
+      <div
+        onClick={handleScroll}
+        className="p-4 bg-white rounded-full text-gray-400 opacity-75 cursor-pointer flex items-center justify-center"
+      >
+        <BsChevronDown size={20} />
+      </div>
       <a
         href="https://wa.me/541170985000" // Reemplaza "541170985000" con el número de WhatsApp al que quieres enviar mensajes
         target="_blank"
         rel="noopener noreferrer"
-        className=" rounded-full text-white cursor-pointer flex items-center justify-center"
+        className="p-4 bg-green-500 rounded-full text-white cursor-pointer flex items-center justify-center"
       >
-      <img src={wsp} size={44}/>
+        <img src={wsp} alt="WhatsApp" className="w-8 h-8" />
       </a>
     </div>
   );
